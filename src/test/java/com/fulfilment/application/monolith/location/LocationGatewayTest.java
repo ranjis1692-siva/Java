6ec023b0d804973040fc9b012df6700a03cb4000
Sivaranjani <sivaranjani.sivachidambaram@jaam.co>
2026-01-29 09:43:56 +0100

    Testcase added for Location Gateway

--- src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
+++ src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -1,18 +1,28 @@
 package com.fulfilment.application.monolith.location;
 
+import com.fulfilment.application.monolith.warehouses.domain.models.Location;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LocationGatewayTest {
 
   @Test
   public void testWhenResolveExistingLocationShouldReturn() {
-    // given
-    // LocationGateway locationGateway = new LocationGateway();
+    LocationGateway locationGateway = new LocationGateway();
+    Location result = locationGateway.resolveByIdentifier("ZWOLLE-001");
+    assertEquals("ZWOLLE-001", result.identification);
+    assertEquals(1, result.maxNumberOfWarehouses);
+    assertEquals(40, result.maxCapacity);
+  }
 
-    // when
-    // Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
-
-    // then
-    // assertEquals(location.identification, "ZWOLLE-001");
+  @Test
+  public void testWhenResolveNonExistingLocationShouldReturn() {
+    LocationGateway locationGateway = new LocationGateway();
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      locationGateway.resolveByIdentifier("NONEXISTENT-001");
+    });
+    assertEquals("Location with identifier 'NONEXISTENT-001' not found", exception.getMessage());
   }
 }
